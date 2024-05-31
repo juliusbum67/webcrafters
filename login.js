c2be482183a70105adaab44a09c15acb980f63cd
Leander Steffan <lenniekeris@gmail.com>
2024-05-31 23:44:54 +0200

    big changes

--- login.js
+++ login.js
@@ -2,6 +2,10 @@ var objPeople = [
     {
         username: "leander",
         password: "leander123"
+    },
+    {
+        username: "julian",
+        password: "julian123"
     }
 ]
 function getInfo() {
@@ -11,7 +15,7 @@ function getInfo() {
     for (var i = 0; i < objPeople.length;i++) {
         if (username == objPeople[i].username && password == objPeople[i].password) {
             console.log(username + "is logged in.")
-            window.location.href = "main.html";
+            window.location.href = "loggedin.html?username=" + encodeURIComponent(username);
             return
         }
     }
